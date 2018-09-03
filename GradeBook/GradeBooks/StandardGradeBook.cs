5b4b89514647123d2ff068cc78008fc04eccb0f6
ESHOPWORLD\bkenny <bkenny@eshopworld.com>
2018-09-03 14:25:32 +0100

    Test
    Check

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = StandardGradeBook.Standard;
+        }
+
+        public static GradeBookType Standard { get; }
+    }
+}
